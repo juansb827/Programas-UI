ba73406c10a85e5662350fcde7da6ffa9bc408b4
Juan Bedoya <juansb827@gmail.com>
2018-11-24 16:54:06 -0500

    Boton Navbar mobile

--- programa.js
+++ programa.js
@@ -1,51 +1,123 @@
-
 //http://leafo.net/sticky-kit/
 $(document).ready(function() {
+
   var elementPosition = 0; //Posición inicial del
   var maxHeight = 0;
   var minHeight = 0;
   var navHeight = 0;
   var stickyOffset = 0; //Distancia que se deben empujar hacia abajo los items fijos
-  var currentMode = "DESKTOP";
+  var offsetHeader = 0;
+  var offsetNavbar = 0;
+  var currentMode = "";
   var shrinked = false;
+  var open = true;  
+  var navbarOpen = true; 
+    
 
-  var programComponent = $('.program-component')
+  var programComponent = $(".program-component");
   var header = $(".program-component .prog-header");
+  var toggleNavbarBtn = $(".program-component .toggle-sidedrawer");
+  var toggleHeaderBtn = $(".program-component .toggle-header-mobile");
   var nav = $(".program-component nav");
-  var stickyElement =  $('.mock-sticky');
+  var stickyElement = $(".mock-sticky");
 
   
   init();
-  //elementPosition = $(".prog-header").offset();
-
-  //initJS();
-  //listenForModeChange();
+  nav.addClass('transitions');
 
   function init() {
-    elementPosition = $(".prog-header").offset();
-    checkSize();
     onWindowResize();
-    $(window).resize(onWindowResize);
-    
-    updateHeaderHeightV2();
-    $(window).scroll(updateHeaderHeightV2);
 
-    function onWindowResize() {   
-      shrinked = false;
-      checkSize();
+    $(window).resize(function() {
+      clearTimeout($.data(this, "resizeTimer"));
+      $.data(
+        this,
+        "resizeTimer",
+        setTimeout(function() {
+          onWindowResize();
+        }, 250)
+      );
+    });
+
+    toggleHeaderBtn.click(function () {
+      open = !open;
+      toggleHeader(open);
       
-      $(".program-component nav").trigger("sticky_kit:detach");
-      $(".program-component .prog-header").trigger("sticky_kit:detach");
+    });
 
-      $(".program-component nav").outerHeight(navHeight + "px");
-      $(".program-component .prog-header").stick_in_parent({
-        offset_top: stickyOffset
+    toggleNavbarBtn.click(function () {
+      navbarOpen = !navbarOpen;
+      toggleNavbar(navbarOpen);
+    })
+
+    $(window).scroll(function() {
+      if (currentMode === "MOBILE") {
+        onScrollMobile();
+      } else {
+        onScrollDesktop();
+      }
+    });
+
+    function onScrollDesktop() {
+      updateHeaderHeightV2();
+    }
+
+    function onScrollMobile() {      
+      if ($(".program-component .prog-header").css("position") == "fixed") {
+        toggleHeaderBtn.show();
+      } else {
+        toggleHeaderBtn.hide();
+      }
+    }
+
+    function onWindowResize() {
+      shrinked = false;
+
+      nav.trigger("sticky_kit:detach");
+      header.trigger("sticky_kit:detach");
+
+      var newMode = $("#mobile-indicator").is(":visible")
+        ? "MOBILE"
+        : "DESKTOP";
+
+      if (newMode !== currentMode && newMode === 'DESKTOP') { //Cambio a DESKTOP
+        disableMobile(); //
+      }
+
+      checkSize(newMode);
+      nav.outerHeight(navHeight + "px");
+
+      header.stick_in_parent({
+        offset_top: offsetHeader
       });
 
-      $(".program-component nav").stick_in_parent({
-        offset_top: minHeight + stickyOffset
-      }); 
-     
+      nav.stick_in_parent({
+        offset_top: offsetNavbar
+      });
+
+      if (newMode !== currentMode && newMode === 'MOBILE') { //Cambio a MOBILE
+          navbarOpen = false;
+          open = false;
+          toggleHeader(false);  
+          toggleNavbar(false);  
+          nav.trigger("sticky_kit:detach");
+          nav.css('position', 'fixed');
+          nav.css('top', offsetNavbar);
+          setTimeout(() => {
+            $(document.body).trigger("sticky_kit:recalc");
+            if (
+              $(".program-component .prog-header").css("position") === "fixed"
+            ) {
+              toggleHeaderBtn.show();
+            }
+          }, 0);
+        
+      }
+      currentMode = newMode;
+      if (currentMode === 'DESKTOP') {
+        updateHeaderHeightV2();
+      }
+      
     }
 
     function updateHeaderHeightV2() {
@@ -57,220 +129,108 @@ $(document).ready(function() {
         st,
         stickyOffset
       );
-  
+
       if (newHeight === minHeight) {
-        $(".prog-header ").addClass("shrinked");
+        header.addClass("shrinked");
         if (!shrinked) {
-          shrinked = true;         
+          shrinked = true;
         }
       } else {
-        
-        $(".prog-header ").removeClass("shrinked");
+        header.removeClass("shrinked");
       }
-  
-      $(".prog-header").outerHeight(newHeight + "px");
+
+      header.outerHeight(newHeight + "px");
       /*programComponent.css('padding-top', newHeight + "px"); */
 
-      if(shrinked === true){
+      if (shrinked === true) {
         $(document.body).trigger("sticky_kit:recalc");
         shrinked = false;
       }
-      
     }
-  }
-
- 
-
-  function listenForModeChange() {
-    $(window).resize(function() {
-      var newMode = $("#mobile-indicator").is(":visible")
-        ? "MOBILE"
-        : "DESKTOP";
-      if (newMode !== currentMode) {
-        switchMode(newMode);
-      }
-    });
-  }
-
-  function initJS() {
-    var mode = $("#mobile-indicator").is(":visible") ? "MOBILE" : "DESKTOP";
-    switchMode(mode);
 
    
-
   }
 
-  function switchMode(newMode) {
-    console.log("Switchted to", newMode);
-    currentMode = newMode;
-    checkSize(currentMode);
-    //Remueve todos los listeners que el modo anterior pudo haber agregado
-    $(".program-component .prog-header").trigger("sticky_kit:detach");
-    $("nav").trigger("sticky_kit:detach");
-    $(window)
-      .off("resize")
-      .off("scroll");
-    $(".program-component .toggle-header-mobile").off("click");
-    $("body").css("overflow", "auto");
-    if (currentMode === "MOBILE") {
-      initMobile();
-    } else {
-      $("nav").outerHeight(navHeight + "px");
-      initDesktop();
-    }
-    listenForModeChange(); //
-  }
+
 
   function checkSize(mode) {
+    stickyOffset = stickyElement[0].offsetTop + stickyElement.outerHeight();
+    elementPosition = header.offset();
+    offsetHeader = stickyOffset;
+    minHeight = parseInt(header.css("min-height").slice(0, -2));
     if (mode === "MOBILE") {
-      //En mobile ocupa toda la pantalla
-      maxHeight = $(window).height() - stickyOffset;
+      //En mobile ignora el CSS
+      header.css("max-height", "calc(100vh - " + stickyOffset + "px)");      
+      navHeight = $(window).height() - stickyOffset;
+      offsetNavbar = stickyOffset;
     } else {
-      //En Desktop Segun el Css
-      maxHeight = parseInt(
-        header
-          .css("max-height")
-          .slice(0, -2)
-      );
+      //Elimina el style max-height que se pudo haber colocado en mobile
+      //para que tome el max-height que esta en CSS
+      header.css("max-height", "");
+      navHeight = $(window).height() - minHeight - stickyOffset;
+      offsetNavbar = stickyOffset + minHeight;
     }
 
-    minHeight = parseInt(
-      header
-        .css("min-height")
-        .slice(0, -2)
-    );
-
-    stickyOffset =  stickyElement[0].offsetTop  + stickyElement.outerHeight();
-    navHeight = $(window).height() - minHeight - stickyOffset;
+    maxHeight = header.css("max-height").slice(0, -2);
+    
+    
+    
   }
 
-  function initDesktop() {
-    $("nav").stick_in_parent({
-      offset_top: minHeight + stickyOffset
-    });
 
-    $(window).resize(function() {
-      clearTimeout($.data(this, "resizeTimer"));
+  function disableMobile() {
+    $(toggleHeaderBtn).hide();
+    $("body").css("overflow", "auto");
+    header.css("max-height", "");
+    nav.removeClass('closed');
+    $(".program-component .prog-header .content-wrapper").scrollTop(0);
+  }
 
-      $.data(
-        this,
-        "resizeTimer",
-        setTimeout(function() {
-          if (currentMode !== "DESKTOP") return;
-          //Ajuste el navbar cuando el tamaño de pantalla cambia
-          checkSize(currentMode);
-          $(".program-component nav").outerHeight(navHeight + "px");
-          $(".program-component nav").trigger("sticky_kit:detach");
-          $(".program-component .prog-header").trigger("sticky_kit:detach");
+  function toggleNavbar(open) { 
+    if (!open) {
+      nav.addClass('closed')
+    }else{
+      nav.removeClass('closed')
+    }       
+  }     
 
-          $(".program-component nav").stick_in_parent({
-            offset_top: minHeight + stickyOffset
-          });
+  function toggleHeader(open) {      
 
-          $(".program-component .prog-header").stick_in_parent({
-            offset_top: stickyOffset
-            //bottoming: false
-          });
+    if (!open) {
+      $(".program-component .prog-header .content-wrapper").scrollTop(0);
+      $(".prog-header")
+        .outerHeight(minHeight)
+        .addClass("shrinked");
+      $("body").css("overflow", "auto");
+    } else {
+      $(".prog-header")
+        .css("height", "calc(100vh - " + stickyOffset + "px)")
+        .removeClass("shrinked");
+      $("body").css("overflow", "hidden");
+    }
+  }
 
-          updateHeaderHeight();
-        }, 250)
-      );
-    });
-
-    updateHeaderHeight();
-    $(window).scroll(updateHeaderHeight);
-    $(".program-component .prog-header").stick_in_parent({
-      offset_top: stickyOffset
-
-      //bottoming: false
-    });
-
-    function updateHeaderHeight() {
-      var st = $(window).scrollTop();
-      var newHeight = calculateHeight(
-        maxHeight,
-        minHeight,
-        elementPosition,
-        st,
-        stickyOffset
-      );
-
-      if (newHeight === minHeight) {
-        $(".prog-header ").addClass("shrinked");
-      } else {
-        $(".prog-header ").removeClass("shrinked");
+  function calculateHeight(
+    initialHeight,
+    minHeight,
+    position,
+    scrollTop,
+    stickyOffset
+  ) {
+    if (scrollTop > position.top - stickyOffset) {
+      //Calcula la nueva altura en funcion de la distancia scrolleada despues de su posicion inicial
+      var newHeight = initialHeight - (scrollTop - position.top + stickyOffset);
+  
+      if (newHeight <= minHeight) {
+        return minHeight;
       }
-
-      $(".prog-header").outerHeight(newHeight + "px");
+  
+      return newHeight;
     }
-
-   
+  
+    return initialHeight;
   }
 
-  function initMobile() {
-    var open = true;
-    $(window).resize(function() {
-      // checkSize(currentMode);
-    });
-
-    checkSize(currentMode);
-    toggleHeader();
-    var toggleHeaderBtn = $(".program-component .toggle-header-mobile");
-    $(".program-component .prog-header")
-      .stick_in_parent({
-        offset_top: stickyOffset,
-        recalc_every: 10
-      })
-      .on("sticky_kit:stick", function(e) {
-        toggleHeaderBtn.show();
-      })
-      .on("sticky_kit:unstick", function(e) {
-        toggleHeaderBtn.hide();
-      })
-      .on("sticky_kit:bottom", function(e) {
-        toggleHeaderBtn.hide();
-      });
-
-    toggleHeaderBtn.click(toggleHeader);
-
-    function toggleHeader() {
-      open = !open;
-
-      if (!open) {
-        $(".program-component .prog-header .content-wrapper").scrollTop(0);
-        $(".prog-header")
-          .outerHeight(minHeight)
-          .addClass("shrinked");
-        $("body").css("overflow", "auto");
-      } else {
-        $(".prog-header")
-          .outerHeight(maxHeight)
-          .removeClass("shrinked");
-        $("body").css("overflow", "hidden");
-      }
-    }
-  }
+  
 });
 
-
-function calculateHeight(
-  initialHeight,
-  minHeight,
-  position,
-  scrollTop,
-  stickyOffset
-) {
-  if (scrollTop > position.top - stickyOffset) {
-    //Calcula la nueva altura en funcion de la distancia scrolleada despues de su posicion inicial
-    var newHeight =
-      initialHeight - (scrollTop - position.top + stickyOffset);
-
-    if (newHeight <= minHeight) {
-      return minHeight;
-    }
-
-    return newHeight;
-  }
-
-  return initialHeight;
-}
