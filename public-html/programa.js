98108dee4215cace58680e9445dcf5ecc9cbe60f
Juan Sebastian Bedoya Arevalo <js.bedoya170@uniandes.edu.co>
2019-02-01 14:09:35 -0500

    Comentarios

--- public-html/programa.js
+++ public-html/programa.js
@@ -1,46 +1,56 @@
 $(document).ready(function() {
- 
+  /**
+   * Variables del estado de la UI,
+   * se usan para determinar los efectos visuales
+   * se calculan cuando el componente se inicializa y tambien cada vez que el tamaño de pantalla cambia
+   *  */
   var elementPosition = 0; //Posición inicial del
   var maxHeight = 0;
   var minHeight = 0;
   var navHeight = 0;
-  var stickyOffset = 0; //Distancia que se deben empujar hacia abajo los items fijos
-  var offsetHeader = 0;
+
+  //Corrimiento del efecto "sticky", es necesario para que el header de este componente no
+  //cubra los elementos fijos de la página (e.g header uniandes)
+  var stickyOffset = 0;
+  //Distancia despues de la cual el MenuLateral se vuelve Fijo
   var offsetNavbar = 0;
+  //Modo Actual, Desktop o Mobile
   var currentMode = "";
+  //Si el header esta a su altura minima
   var shrinked = false;
-  var open = true;
-  var navbarOpen = true;
+  //Si el menu lateral esta abierto
+  var navbarOpen = false;
 
   var programComponent = $(".program-component");
   var header = $(".program-component .prog-header");
   var toggleNavbarBtn = $(".program-component .toggle-sidedrawer");
-  var toggleHeaderBtn = $(".program-component .toggle-header-mobile");
+
   var nav = $(".program-component nav");
   var stickyElement = $(componentProps.stickyElementSelector);
 
-  var items = $(".program-component .content .content-item");
-  var contentItems = [];
-  for (var i = 0; i < items.length; i++) {
-    contentItems.push($(items[i]));
-  }
-
+  //Headers de los items de la sección de contenido
+  var contentHeaders = $(".program-component .content .item-header")
+    .toArray()
+    .map($);
+  
+  //Lista items del menu
   var menuItemsList = $(".menu-container .menu-item");
-  var menuItemById = {};
+  var menuItemsById = {}; //menuItemsList organizado en un Map
 
   function initMenuItems() {
+    //Inicializa el Map menuItemsById
     for (var i = 0; i < menuItemsList.length; i++) {
       var menuItem = $(menuItemsList[i]);
       var dataId = menuItem.attr("data-target");
-      menuItemById[dataId] = menuItem;
+      menuItemsById[dataId] = menuItem;
     }
 
     $(".menu-container .menu-item .item-name").click(onItemClickHandler);
     $(".menu-container .menu-item .sub-item").click(onItemClickHandler);
 
+    
     function onItemClickHandler() {
-      var dataId = $(this).attr("data-target");
-      console.log("onItemClickHandler", dataId);
+      var dataId = $(this).attr("data-target");      
       var contentOffset = $(".content #" + dataId).offset();
       if (contentOffset) {
         $("html, body").animate(
@@ -52,23 +62,23 @@ $(document).ready(function() {
       }
     }
   }
-
   initMenuItems();
   init();
   initBackgroundColor();
   nav.addClass("transitions");
- 
 
-
+  /**
+   * LightBox con video
+   * */
   $(".program-component .prog-media-container").click(function() {
+    lity(componentProps.videoURL);
     if ($(this).css("opacity") !== "0") {
-      lity(componentProps.videoURL);
     }
   });
 
   function init() {
     onWindowResize();
-
+    /** Ignora  */
     $(window).resize(function() {
       clearTimeout($.data(this, "resizeTimer"));
       $.data(
@@ -80,225 +90,178 @@ $(document).ready(function() {
       );
     });
 
-    toggleHeaderBtn.click(function() {
-      open = !open;
-      toggleHeader(open);
-    });
-
     toggleNavbarBtn.click(function() {
-      navbarOpen = !navbarOpen;
-      toggleNavbar(navbarOpen);
+      toggleNavbar(!navbarOpen);
     });
+  }
 
-    function scrollListeners() {
-      $(window).scroll(handleScroll);
-      function handleScroll() {
-        if (currentMode === "MOBILE") {
-          //onScrollMobile();
-        } else {
-          onScrollDesktop();
-        }
-        updateActiveItem();
-      }
-    }
-    scrollListeners();
-
-    function updateActiveItem() {
-      var activeItem = getActiveItem(
-        $(window).scrollTop(),
-        contentItems,
-        stickyOffset + minHeight + 100
-      );
-      var dataId = activeItem.attr("id");
-      if (menuItemById[dataId]) {
-        var prevActive = $(".menu-container .menu-item.active").attr('data-target');
-        if (prevActive !== dataId) { //Scroll a una nueva seccion
-          console.log('active item changed');
-          $('.navigation-menu .menu-toggle').addClass('animate');
-          setTimeout(function(){
-            $('.navigation-menu .menu-toggle').removeClass('animate');
-          }, 500)
-          
-        }
-        menuItemsList.removeClass("active");
-        menuItemById[dataId].addClass("active");
-      }
-    }
-
-    function getActiveItem(scrollTop, items, offset) {
-      scrollTop += offset;
-      var activeItem = items[0];
-
-      for (var i = 1; i < items.length; i++) {
-        var item = items[i];
-        var distanceFromTop = item.offset().top;
-        if (distanceFromTop <= scrollTop) {
-          activeItem = item;
-        } else {
-          break;
-        }
-      }
-
-      return activeItem;
-    }
-
-    function onScrollDesktop() {
+  function registerScrollListeners() {
+    $(window).scroll(function() {
+      
       updateHeaderHeight();
-    }
-    /*
-    function onScrollMobile() {
-      if ($(".program-component .prog-header").css("position") == "fixed") {
-        toggleHeaderBtn.show();
-      } else {
-        toggleHeaderBtn.hide();
-      }
-    }*/
-
-    function onWindowResize() {
-      shrinked = false;
-      $(window).off("scroll");
-      header.trigger("sticky_kit:detach");
-      nav.trigger("sticky_kit:detach");
-
-      var newMode = $("#mobile-indicator").is(":visible")
-        ? "MOBILE"
-        : "DESKTOP";
-
-      checkSize(newMode);
-      nav.outerHeight(navHeight + "px");
-
-      if (newMode === "DESKTOP") {
-        //Cambio a DESKTOP
-        header.stick_in_parent({
-          offset_top: offsetHeader,
-          recalc_every: null //newMode === 'DESKTOP' ? null : null
-        });
-        disableMobile();
-
-        nav.stick_in_parent({
-          offset_top: offsetNavbar
-        });
-      } else {
-        console.log("here");
-        nav.css("position", "fixed");
-        nav.css("top", offsetNavbar);
-      }
-
-      scrollListeners();
-
-      if (newMode === "MOBILE") {
-        //Cambio a MOBILE //newMode !== currentMode  &&
-        navbarOpen = false;
-        open = false;
-
-        toggleNavbar(false);
-
-        setTimeout(function() {
-          if (
-            $(".program-component .prog-header").css("position") === "fixed"
-          ) {
-            toggleHeaderBtn.show();
-          }
-        }, 0);
-      }
-      currentMode = newMode;
-
-      if (currentMode === "DESKTOP") {
-        updateHeaderHeight();
-      }
-      setTimeout(function() {
-        $(document.body).trigger("sticky_kit:recalc");
-      });
       updateActiveItem();
-    }
-
-    function updateHeaderHeight() {
-      var st = $(window).scrollTop();
-      var newHeight = calculateHeight(
-        maxHeight,
-        minHeight,
-        elementPosition,
-        st,
-        stickyOffset
+     
+    });
+  }
+  /** Actualiza el item activo (resaltado) del menu*/
+  function updateActiveItem() {
+    var activeItem = getActiveItem(
+      $(window).scrollTop(),
+      contentHeaders,
+      stickyOffset + minHeight + 100
+    );
+    var dataId = activeItem.attr("id");
+    if (menuItemsById[dataId]) {
+      var prevActive = $(".menu-container .menu-item.active").attr(
+        "data-target"
       );
-
-      if (newHeight === minHeight) {
-        header.addClass("shrinked");
-
-        if (!shrinked) {
-          shrinked = true;
-          setTimeout(function() {
-            $(document.body).trigger("sticky_kit:recalc");
-          }, 0);
-
-          console.log("recalc");
-        }
-      } else {
-        header.removeClass("shrinked");
+      if (prevActive !== dataId) {
+        //Scroll a una nueva seccion
+        console.log("active item changed");
+        $(".navigation-menu .menu-toggle").addClass("animate");
+        setTimeout(function() {
+          $(".navigation-menu .menu-toggle").removeClass("animate");
+        }, 500);
       }
-
-      header.outerHeight(newHeight + "px");
-      /*programComponent.css('padding-top', newHeight + "px"); */
+      menuItemsList.removeClass("active");
+      menuItemsById[dataId].addClass("active");
     }
   }
 
-  function checkSize(mode) {
+  function onWindowResize() {
+    shrinked = false;
+
+    //Remueve los listeners
+    $(window).off("scroll");
+    header.trigger("sticky_kit:detach");
+    nav.trigger("sticky_kit:detach");
+    //Determina el nuevo modo
+    var newMode = $("#mobile-indicator").is(":visible") ? "MOBILE" : "DESKTOP";
+
+    updateVars(newMode);
+    console.log("height", navHeight);
+    nav.css("height", navHeight);
+
+    if (newMode === "DESKTOP") {
+      //Cambio a DESKTOP
+
+      header.stick_in_parent({
+        offset_top: stickyOffset,
+        recalc_every: null //newMode === 'DESKTOP' ? null : null
+      });
+      header.css("max-height", "");
+      nav.removeClass("closed");
+      header.css("position", "");
+
+      nav.stick_in_parent({
+        offset_top: offsetNavbar
+      });
+    } else {
+      //Cambio a MOBILE
+      nav.css("position", "fixed");
+      nav.css("top", offsetNavbar);
+    }
+
+    registerScrollListeners();
+
+    if (newMode === "MOBILE") {
+      toggleNavbar(false);
+
+      setTimeout(function() {
+        if ($(".program-component .prog-header").css("position") === "fixed") {
+          toggleHeaderBtn.show();
+        }
+      }, 0);
+    }
+    currentMode = newMode;
+
+    if (currentMode === "DESKTOP") {
+      updateHeaderHeight();
+    }
+    setTimeout(function() {
+      $(document.body).trigger("sticky_kit:recalc");
+    });
+    updateActiveItem();
+  }
+
+  /** 
+   * Actualiza la altura del header segun la posicion del scroll 
+   * */
+  function updateHeaderHeight() {
+    var st = $(window).scrollTop();
+    var newHeight = calculateHeight(
+      maxHeight,
+      minHeight,
+      elementPosition,
+      st,
+      stickyOffset
+    );
+
+    if (newHeight <= minHeight) {
+      header.addClass("shrinked");
+      if (!shrinked) {
+        shrinked = true;
+        setTimeout(function() {
+          $(document.body).trigger("sticky_kit:recalc");
+        }, 0);
+      }
+    } else {
+      header.removeClass("shrinked");
+    }
+
+    header.outerHeight(newHeight + "px");
+  }
+
+  /**
+   * Actualiza las variables del estado de la UI
+   */
+  function updateVars(mode) {
     stickyOffset = stickyElement[0].offsetTop + stickyElement.outerHeight();
     elementPosition = header.offset();
-    offsetHeader = stickyOffset;
     minHeight = parseInt(header.css("min-height").slice(0, -2));
+
     if (mode === "MOBILE") {
       header.css("max-height", "auto");
       header.css("height", "auto");
-      navHeight = $(window).height() - stickyOffset;
+      navHeight = "calc(100vh - " + stickyOffset + "px)";
       offsetNavbar = stickyOffset;
       programComponent.css("padding-top", "");
       minHeight = 0;
-      /*
-      minHeight =
-        $(".content-wrapper .prof-title").outerHeight() +
-        $(".prog-header .toggle-header-mobile").outerHeight();
-      header.css("min-height", minHeight + "px");
-
-      header.css("max-height", "calc(100vh - " + stickyOffset + "px)");
-      programComponent.css("padding-top", "");
-      
-      */
     } else {
       //DESKTOP
-      //La altura minima se ajusta a la cantidad de texto del titulo
+
+      //La altura minima del header se ajusta a la cantidad de texto del titulo
       minHeight =
         $(".content-wrapper .prof-title").outerHeight() +
         $(".prog-header .prog-data").outerHeight();
-      //Si la altura maxima del header (configurada en css) es mayor al alto
-      //viewport, esta se reajusta
       header.css("min-height", minHeight + "px");
+
+      //La altura maxima del header (configurada en css) se reajusta si es mayor al viewport
       header.css("max-height", "calc(100vh - " + stickyOffset + "px)");
-      var calculatedMax = parseInt(header.css("max-height").slice(0, -2));
-      header.css("max-height", "");
+      var viewPortHeight = parseInt(header.css("max-height").slice(0, -2)); //Altura del viewport
+      header.css("max-height", ""); //
       var cssMaxHeight = parseInt(header.css("max-height").slice(0, -2));
-      if (calculatedMax <= cssMaxHeight) {
+      programComponent.css("padding-top","");
+      if (cssMaxHeight > viewPortHeight) {
         //Altura de css sobrepasa el alto de viewport
-        header.css("max-height", calculatedMax + "px");
-        programComponent.css("padding-top", calculatedMax + "px");
+        header.css("max-height", "calc(100vh - " + stickyOffset + "px)");     
+        //El header siempre tendra position: absolute o fixed, entonces es necesario que
+        //el padding-top de programComponent la altura del header.
+        programComponent.css(
+          "padding-top",
+          "calc(100vh - " + stickyOffset + "px)"
+        );
       }
-      navHeight = $(window).height() - minHeight - stickyOffset;
+      navHeight = "calc(100vh - " + minHeight + "px - " + stickyOffset + "px)";     
       offsetNavbar = stickyOffset + minHeight;
     }
 
     maxHeight = parseInt(header.css("max-height").slice(0, -2));
   }
 
-  function disableMobile() {
-    $(toggleHeaderBtn).hide();
-    $("body").css("overflow", "auto");
-    header.css("max-height", "");
-    nav.removeClass("closed");
-    header.css("position", "");
-    //$(".program-component .prog-header .content-wrapper").scrollTop(0);
-  }
-
   function toggleNavbar(open) {
+    
+    navbarOpen = open;
     if (!open) {
       header.addClass("nav-closed");
       nav.addClass("closed");
@@ -307,93 +270,106 @@ $(document).ready(function() {
       header.removeClass("nav-closed");
     }
   }
-
-  function toggleHeader(open) {
-    if (!open) {
-      $(".program-component .prog-header .content-wrapper").scrollTop(0);
-      $(".prog-header")
-        .outerHeight(minHeight)
-        .addClass("shrinked");
-      $("body").css("overflow", "auto");
-    } else {
-      $(".prog-header")
-        .css("height", "calc(100vh - " + stickyOffset + "px)")
-        .removeClass("shrinked");
-      $("body").css("overflow", "hidden");
-    }
-  }
-
-  function calculateHeight(
-    initialHeight,
-    minHeight,
-    position,
-    scrollTop,
-    stickyOffset
-  ) {
-    if (scrollTop > position.top - stickyOffset) {
-      //Calcula la nueva altura en funcion de la distancia scrolleada despues de su posicion inicial
-      var newHeight = initialHeight - (scrollTop - position.top + stickyOffset);
-
-      if (newHeight <= minHeight) {
-        return minHeight;
-      }
-
-      return newHeight;
-    }
-
-    return initialHeight;
-  }
-
-  function initBackgroundColor() {
-     
-    $.adaptiveBackground.run({
-      parent: "header",
-      success: function($img, data) {
-        
-        //console.log("Success!", $img, data);
-       var rgbColor = data.color.slice(4, -1).split(',');
-       //Usa como color de fuente el color mas lejano (negro o blanco) 
-       //del color dominate de la imagen
-       var contrast = getFurthestColor(rgbColor, [[255, 255, 255], [0, 0, 0]]);
-       contrast = 'rgb('+ contrast.join() +')';
-       
-       $(".prog-media-container .video-icon, .program-component .prog-header, .prog-data .data-item")
-        .css("color", contrast );
-        $('.prog-data .data-item').css("border-color", contrast );
-        $(".bottom-gradient").css(
-          "background-image",
-          "linear-gradient( to bottom, rgba(255, 255, 255, 0), " + data.color
-        );
-  
-        $(".left-gradient").css(
-          "background-image",
-          "linear-gradient( to left, rgba(255, 255, 255, 0), " + data.color
-        );
-      }
-    });
-  }
-  /**
-   * Retorna el color que este mas lejano de baseColor usando
-   * https://en.wikipedia.org/wiki/Color_difference
-   */
-  function getFurthestColor(color, colors) {
-     var r1 = color[0];
-     var g1= color[1];
-     var b1 = color[2];
-     var max = Number.MIN_VALUE;
-     var furthestColor = color;
-     colors.forEach(function(c) {
-       var r2 = c[0];
-       var g2 = c[1];
-       var b2 = c[2];
-       var distance = Math.pow(r2 - r1, 2) + Math.pow(g2 - g1, 2) + Math.pow(b2 - b1, 2);
-       if (distance > max) {
-         max = distance;
-         furthestColor = c;
-       }
-     });
-     return furthestColor;
-    //.slice(4, -1).split(',')
-  }
-
 });
+
+/**
+ * Determina cual de los items es el activo segun  la posicion del scroll y la posicion de los headers
+ * @param headers  Lista de headers de la seccion de contenido
+ */
+function getActiveItem(scrollTop, headers, offset) {
+  scrollTop += offset;
+  var activeItem = headers[0];
+
+  for (var i = 1; i < headers.length; i++) {
+    var item = headers[i];
+    var distanceFromTop = item.offset().top;
+    if (distanceFromTop > scrollTop) {
+      break; //El header activo se encontró en la iteración anterior
+    }
+    activeItem = item;
+  }
+
+  return activeItem;
+}
+
+/**
+ * Calcula la nueva altura del header en funcion de la distancia scrolleada
+ * @param initialHeight - altura original del header
+ * @param initialHeight - altura minima header (despues de cierta distancia el header no se encogera más)
+ * @param position - posicion del header respecto al inicio de la página
+ * @param scrollTop - posicion del scroll respecto al inicio de la página
+ * @param stickyOffset - corrimiento,
+ */
+function calculateHeight(
+  initialHeight,
+  minHeight,
+  position,
+  scrollTop,
+  stickyOffset
+) {
+  if (scrollTop > position.top - stickyOffset) {
+    var newHeight = initialHeight - (scrollTop - position.top + stickyOffset);
+    
+    return Math.max(minHeight, newHeight);
+  }
+  return initialHeight;
+}
+
+/**
+ * Usa la libreria https://github.com/briangonzalez/jquery.adaptive-backgrounds.js
+ * Calcula el color dominante de la imagen del header y lo pone de fondo
+ * TODO: PASAR LÓGICA A DRUPAL
+ */
+function initBackgroundColor() {
+  $.adaptiveBackground.run({
+    parent: "header",
+    success: function($img, data) {
+      //console.log("Success!", $img, data);
+      var rgbColor = data.color.slice(4, -1).split(",");
+      //Usa como color de fuente, el color mas lejano (negro o blanco)
+      //del color dominate de la imagen
+      var contrast = getFurthestColor(rgbColor, [[255, 255, 255], [0, 0, 0]]);
+      contrast = "rgb(" + contrast.join() + ")";
+
+      $(
+        ".prog-media-container .video-icon, .program-component .prog-header, .prog-data .data-item"
+      ).css("color", contrast);
+      $(".prog-data .data-item").css("border-color", contrast);
+      $(".bottom-gradient").css(
+        "background-image",
+        "linear-gradient( to bottom, rgba(255, 255, 255, 0), " + data.color
+      );
+
+      $(".left-gradient").css(
+        "background-image",
+        "linear-gradient( to left, rgba(255, 255, 255, 0), " + data.color
+      );
+    }
+  });
+}
+
+/**
+ * Retorna el color que este mas lejano del color base usando
+ * https://en.wikipedia.org/wiki/Color_difference
+ * @param color - color base
+ * @param colors lista de colores contra los cuales se compara el color base
+ */
+function getFurthestColor(color, colors) {
+  var r1 = color[0];
+  var g1 = color[1];
+  var b1 = color[2];
+  var max = Number.MIN_VALUE;
+  var furthestColor = color;
+  colors.forEach(function(c) {
+    var r2 = c[0];
+    var g2 = c[1];
+    var b2 = c[2];
+    var distance =
+      Math.pow(r2 - r1, 2) + Math.pow(g2 - g1, 2) + Math.pow(b2 - b1, 2);
+    if (distance > max) {
+      max = distance;
+      furthestColor = c;
+    }
+  });
+  return furthestColor;
+}
